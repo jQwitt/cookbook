ebab3da6735a67d54b77c2050ce404060fe49549
Jack Witt <jwitt1452@gmail.com>
2021-12-16 19:04:43 -0800

    added basic create-react-app

---
+++ react.web/src/components/dummy.component.js
@@ -0,0 +1,5 @@
+function DummyComponent() {
+  return <div>Dummy Component</div>;
+}
+
+export default DummyComponent;
---
+++ react.web/src/views/App.js
@@ -0,0 +1,12 @@
+import { DummyComponent } from "../components";
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <DummyComponent />
+    </div>
+  );
+}
+
+export default App;
