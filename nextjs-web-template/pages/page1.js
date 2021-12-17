24481b678897da364552974b0f6908c300d1081d
Jack Witt <jwitt1452@gmail.com>
2021-12-17 12:04:16 -0800

    cleanup nextjs-web-template

---
+++ nextjs-web-template/pages/page1.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "next/link";
+import "tailwindcss/tailwind.css";
+
+export default function Page1() {
+  return (
+    <div className="flex flex-col min-h-screen justify-center items-center">
+      <div className="font-bold text-xl mb-2">Starter App</div>
+      <Link href="/">
+        <a>nextjs default page</a>
+      </Link>
+    </div>
+  );
+}
