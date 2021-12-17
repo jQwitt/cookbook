81b7359a8ab9261086a4a644b2df92c10e396401
Jack Witt <jwitt1452@gmail.com>
2021-12-16 19:49:44 -0800

    added basic kotlin springboot app

---
+++ spring.kotlin.web/src/main/kotlin/com/cookbook/springkotlintemplate/SpringkotlintemplateApplication.kt
@@ -0,0 +1,11 @@
+package com.cookbook.springkotlintemplate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringkotlintemplateApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringkotlintemplateApplication>(*args)
+}
