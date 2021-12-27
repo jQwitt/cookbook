d71fe99d0e30c6f9875dc9cb0cc52b3926935d51
Jack Witt <Jwitt1452@gmail.com>
2021-12-26 16:28:36 -0800

    update cleanup

--- .scripts/cleanAll.zsh
+++ .scripts/cleanAll.zsh
@@ -8,12 +8,16 @@ do
   case $dir in
   /Users/jackwitt/Desktop/cookbook/nextjs|/Users/jackwitt/Desktop/cookbook/node|/Users/jackwitt/Desktop/cookbook/react)
     # cd into project and remove heavy files
-    echo cleaning $dir
-    cd $dir/template
+    cd $dir
+    for subDir in `ls`
+    do
+      echo cleaning $dir/$subDir
+      cd $dir/$subDir
 
-    rm -f yarn.lock
-    rm -f yarn-error.log
-    rm -rf node_modules
+      rm -f yarn.lock
+      rm -f yarn-error.log
+      rm -rf node_modules
+    done
     ;;
   *)
     ;;
