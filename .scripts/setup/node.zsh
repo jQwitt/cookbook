7c4eaae19c4fc9f388568f62ce471f889791ce43
Jack Witt <Jwitt1452@gmail.com>
2022-02-06 14:41:42 -0800

    node setup script

--- .scripts/setup/node.zsh
+++ .scripts/setup/node.zsh
@@ -1,9 +1,12 @@
 #!/usr/bin/env zsh
 
-# if [ ! $1 ]; then
-#   msg='no setup path given'
-#   echo "\e[1;31m$msg\e[1;31m"
-#   exit 1
-# fi
+if [ ! $1 ]; then
+  msg='no setup path given'
+  echo "\e[1;31m$msg\e[1;31m"
+  exit 1
+fi
 
 echo "running setup tasks for $1"
+
+source ~/.nvm/nvm.sh && nvm use
+yarn
