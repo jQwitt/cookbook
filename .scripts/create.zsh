4df54197cfd99331b81adf2d3e6ac6560d530d28
Jack Witt <Jwitt1452@gmail.com>
2022-02-06 13:47:53 -0800

    update create script

--- .scripts/create.zsh
+++ .scripts/create.zsh
@@ -3,21 +3,38 @@
 cd ..
 cookbookPath=$(pwd)
 
-echo Welcome to cookbook!
+echo 'Welcome to cookbook!'
 vared -p 'what should we call your recipie?  ' -c name
+if [ ! $name ]; then
+  msg='cannot cook without a name'
+  echo "\e[1;31m$msg\e[1;31m"
+  exit 1
+fi
 
 vared -p 'what flavor?  ' -c flavor;
 case $flavor in
   nextjs|node|react|spring)
-    # create project next to cookbook as default
+    vared -p 'any spice?  ' -c spice;
+    
+    # see if spice exists, and is valid
+    toCp=$cookbookPath/$flavor/${spice:="template"}
+    if [ ! -d $toCp ]; then
+      msg="invalid recipie $flavor:$spice"
+      echo "\e[1;31m$msg\e[1;31m"
+      exit 1
+    fi  
+
+    msg="creating new $flavor:$spice project at $(pwd)"
+    echo "\e[1;32m$msg\e[1;32m"
     cd ..
     mkdir $name
     cd $name
-
-    echo creating new $flavor project at $(pwd)
-    cp -a $cookbookPath/$flavor/template/. .
+    cp -a $toCp/. .
     ;;
   *)
-    echo Unrecognized flavor: $flavor - please try again
+    msg="unrecognized flavor: ${flavor:-' '}"
+    echo "\e[1;31m$msg\e[0m"
     ;;
 esac
+
+exit 0
