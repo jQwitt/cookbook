a878f94bfa82de0cfa2b22ef9fe23c4a51f7388f
Jack Witt <Jwitt1452@gmail.com>
2022-12-01 09:56:58 -0800

    feat: adding react/ts-tailwindcss-vite template

---
+++ react/ts-tailwindcss-vite/src/components/root/App.tsx
@@ -0,0 +1,10 @@
+import { HelmetProvider } from 'react-helmet-async';
+import Main from '~/components/root/Main';
+
+export const App = () => {
+  return (
+    <HelmetProvider>
+      <Main />
+    </HelmetProvider>
+  );
+};
---
+++ react/ts-tailwindcss-vite/src/components/root/Main.tsx
@@ -0,0 +1,11 @@
+import { Router } from '~/components/router/Router';
+
+function Main() {
+  return (
+    <main>
+      <Router />
+    </main>
+  );
+}
+
+export default Main;
